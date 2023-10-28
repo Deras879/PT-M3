c9602bd198d00dc705e6b10f7ea450b2053cbd76
Deras879 <derycard59@gmail.com>
2023-10-27 22:59:57 -0500

    Finalizado CH 1

--- 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
+++ 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
@@ -4,6 +4,31 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 
+function $Promise(executor){
+    if(typeof executor !== "function") throw new TypeError("El argumento executor no es una function")
+
+    this._state = "pending"
+    this._value = undefined
+    this._internalResolve = (value) => {
+        if(this._state === "fulfilled" || this._state === "rejected") return
+        this._state = "fulfilled"
+        this._value = value
+}
+    this._internalReject = (reason) => {
+        if(this._state === "fulfilled" || this._state === "rejected") return
+        this._state = "rejected";
+        this._value = reason
+    }
+
+    const resolve = (value) => {this._internalResolve(value)}
+    const reject = (reason) => {this._internalReject(reason)}
+
+
+    executor(resolve, reject);
+}
+
+
+
 
 
 module.exports = $Promise;
